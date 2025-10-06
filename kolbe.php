18419032c146bdbca8e2403fdeec7989318e8479
mgolomaximilienmpia <ngolomaxi64@gmail.com>
2025-10-06 18:43:19 +0200

    Add files via upload

---
+++ kolbe.php
@@ -0,0 +1,13 @@
+<?php
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perché";
+$text6 = "hai";
+$text7 = "bevuto";
+$text8 = "tutto";
+
+// Concatenazione corretta
+echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+?>
